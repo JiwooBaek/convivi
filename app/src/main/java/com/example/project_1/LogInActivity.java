9d553f5d1e3edb1ea684b408231f5e8e1dd965a6
syeon-c <gkdltjdus@gmail.com>
2020-09-24 17:43:16 +0900

    로그인페이지 레이아웃 구현

---
+++ app/src/main/java/com/example/project_1/LogInActivity.java
@@ -0,0 +1,48 @@
+package com.example.project_1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LogInActivity extends AppCompatActivity {
+
+    private EditText id;
+    private EditText password;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        id = (EditText) findViewById(R.id.userId);
+        password = (EditText) findViewById(R.id.userPassword);
+
+        Button login = (Button) findViewById(R.id.logIn);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button signUp = (Button) findViewById(R.id.signUp);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
+    }
+}
