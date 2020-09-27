201599cc0919378dc5effc422f99600e356a3257
SJ\Hi <dkssud122103@gmail.com>
2020-09-28 03:05:22 +0900

    MessageActivity 생성과 User 클래스 생성. Circular imageView 코드를 가져옴. 데이터베이스에 유저 정보 담는 란을 임의로 ' Users'로  지정하고 Users 유저 정보를 가져온다고 해뒀습니다. (MessageActivity 43줄)

---
+++ app/src/main/java/com/example/project_1/MessageActivity.java
@@ -0,0 +1,63 @@
+package com.example.project_1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.project_1.Model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MessageActivity extends AppCompatActivity {
+
+    CircleImageView profile_image;
+    TextView username;
+
+    FirebaseUser fuser;
+    DatabaseReference reference;
+
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+
+        profile_image = findViewById(R.id.profile_image);
+        username = findViewById(R.id.userName);
+
+        intent = getIntent();
+        String userid = intent.getStringExtra("userid");
+
+        fuser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                username.setText(user.getUsername());
+                if (user.getImageURL().equals("default")) {
+                    profile_image.setImageResource(R.mipmap.ic_launcher);
+                } else {
+                   // Glide.with(MessageActivity.this).load(user.getImageURL()).into(profile_image); Glide도 이미지 로드 프로그램같은 건 듯 일단 알아보기
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
