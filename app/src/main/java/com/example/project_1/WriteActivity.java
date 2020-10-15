0f5c33a2e4539f772f92eefda8fe42923c438f46
libra980 <libra980@naver.com>
2020-10-15 17:11:53 +0900

    ViewMoreActivity 수정(2)

--- app/src/main/java/com/example/project_1/WriteActivity.java
+++ app/src/main/java/com/example/project_1/WriteActivity.java
@@ -52,21 +52,21 @@ public class WriteActivity extends AppCompatActivity {
         firebaseUser = firebaseAuth.getCurrentUser();
         firebaseUser.reload();
 
-         btn_save.setOnClickListener(new android.view.View.OnClickListener() {
-             @Override
-             public void onClick(android.view.View v) {
-                 ScriptModel scriptModel = new ScriptModel();
-                 uid = firebaseAuth.getCurrentUser().getUid();
+        btn_save.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                ScriptModel scriptModel = new ScriptModel();
+                uid = firebaseAuth.getCurrentUser().getUid();
 
-                 scriptModel.title = et_title.getText().toString();
-                 scriptModel.description = et_description.getText().toString();
-                 scriptModel.imgld = R.drawable.default_1;
+                scriptModel.title = et_title.getText().toString();
+                scriptModel.description = et_description.getText().toString();
+                scriptModel.imgld = R.drawable.default_1;
 
 
-                 FirebaseDatabase.getInstance().getReference().child("").child(uid).setValue(scriptModel);
+                FirebaseDatabase.getInstance().getReference().child("").child(uid).setValue(scriptModel);
 
-             }
-         });
+            }
+        });
 
     }
 
@@ -84,3 +84,109 @@ public class WriteActivity extends AppCompatActivity {
 
 
 }
+
+
+
+/* package com.example.project_1;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.core.view.View;
+
+import java.util.ArrayList;
+
+import model.ScriptModel;
+
+public class WriteActivity extends AppCompatActivity {
+
+    //인스턴스 선언
+    private FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    String uid;
+    EditText et_title;
+    EditText et_description;
+    Button btn_exit;
+    Button btn_save;
+
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write);
+
+        //인스턴스 초기화
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        et_title = findViewById(R.id.et_title);
+        et_description = findViewById(R.id.et_description);
+        btn_exit = findViewById(R.id.btn_exit);
+        btn_save = findViewById(R.id.btn_save);
+
+//        FirebaseUser user = firebaseAuth.getCurrentUser();
+//        if (user != null) {
+//            uid = user.getUid();
+//        }
+
+        firebaseUser = firebaseAuth.getCurrentUser();
+        firebaseUser.reload();
+
+        btn_save.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+                ScriptModel scriptModel = new ScriptModel();
+                uid = firebaseAuth.getCurrentUser().getUid();
+
+                scriptModel.title = et_title.getText().toString();
+                scriptModel.description = et_description.getText().toString();
+                scriptModel.imgld = R.drawable.default_1;
+
+
+                FirebaseDatabase.getInstance().getReference().child("Buy").child(uid).setValue(scriptModel);
+
+            }
+        });
+
+    }
+
+//    public void clickSave(View view){
+//
+//        scriptModel.title = et_title.getText().toString();
+//        scriptModel.description = et_description.getText().toString();
+//        scriptModel.imgld = R.drawable.default_1;
+//
+//
+//        FirebaseDatabase.getInstance().getReference().child("posts").child(uid).setValue(scriptModel);
+//
+//    }
+//
+
+
+}
+
+
+//    public void clickSave(View view){
+//
+//        scriptModel.title = et_title.getText().toString();
+//        scriptModel.description = et_description.getText().toString();
+//        scriptModel.imgld = R.drawable.default_1;
+//
+//
+//        FirebaseDatabase.getInstance().getReference().child("posts").child(uid).setValue(scriptModel);
+//
+//    }
+//
+
+
+
+*/
