c6753f504159257d8aea6e59c9ec5be2833fd4fb
hyu0076 <hyu0076@gmail.com>
2020-10-15 16:05:17 +0900

    플로팅 버튼/ 글쓰기 수정

--- app/src/main/java/com/example/project_1/WriteActivity.java
+++ app/src/main/java/com/example/project_1/WriteActivity.java
@@ -63,7 +63,7 @@ public class WriteActivity extends AppCompatActivity {
                  scriptModel.imgld = R.drawable.default_1;
 
 
-                 FirebaseDatabase.getInstance().getReference().child("").child(uid).setValue(scriptModel);
+                 FirebaseDatabase.getInstance().getReference().child("Buy").child(uid).setValue(scriptModel);
 
              }
          });
