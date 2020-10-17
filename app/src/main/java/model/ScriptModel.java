017d6cbbbbfd6f1790ebfdd569bff82f22767862
syeon-c <gkdltjdus@gmail.com>
2020-10-17 16:35:49 +0900

    작성글 번호 자동 증가 구현

--- app/src/main/java/model/ScriptModel.java
+++ app/src/main/java/model/ScriptModel.java
@@ -3,59 +3,19 @@ package model;
 
 public class ScriptModel {
     public String title;   //제목
+    public String host; //사용자uid
     public String description; // 내용
     public int imgld;  //프로필 이미지?
 
-
+    public ScriptModel() {
     }
-//    public ScriptModel(String uid, String title, String description, int imgld){
-//        this.uid = uid;
-//        this.title = title;
-//        this.description = description;
-//        this.imgld = imgld;
-//    }
-//
-//
-//
-//    public Map<String, Object> toMap() {
-//        HashMap<String, Object> result = new HashMap<>();
-//        result.put("uid", uid);
-//        result.put("title", title);
-//        result.put("description",description);
-//        result.put("imgld", imgld);
-//        return result;
-//    }
-//
-//    public void setUid(String uid) {
-//        this.uid = uid;
-//    }
-//
-//    public void setTitle(String title) {
-//        this.title = title;
-//    }
-//
-//    public void setDescription(String description) {
-//        this.description = description;
-//    }
-//
-//    public void setImgld(int imgld) {
-//        this.imgld = imgld;
-//    }
-//
-//    public String getuid(){
-//        return uid;
-//    }
-//
-//    public String getTitle(){
-//        return title;
-//    }
-//
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public int getImgld(){
-//        return imgld;
-//    }
-//
+
+    public ScriptModel(String title, String host, String description, int imgld) {
+        this.title = title;
+        this.host = host;
+        this.description = description;
+        this.imgld = imgld;
+    }
+}
+
 
