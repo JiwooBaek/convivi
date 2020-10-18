9cfa8c724b6f601703933a0f16520e1ea8e64de8
syeon-c <gkdltjdus@gmail.com>
2020-10-18 16:12:32 +0900

    팝업액티비티 구현 시도2

--- app/src/main/java/com/example/project_1/PopUpActivity.java
+++ app/src/main/java/com/example/project_1/PopUpActivity.java
@@ -1,9 +1,12 @@
 package com.example.project_1;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.Window;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.WindowDecorActionBar;
 
 public class PopUpActivity extends AppCompatActivity {
     @Override
@@ -11,5 +14,23 @@ public class PopUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popup);
 
+        //타이틀바 제거
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //UI 객체 생성
+
+        //데이터 가져오기
+
+        //닫기 버튼 클릭시 팝업 닫기
+
+    }
+
+    //바깥 레이어 클릭시 안닫히게
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_OUTSIDE) {
+            return false;
+        }
+        return true;
     }
 }
