aede99abad39e0f8147122a1eabc0ec4225f5256
Hayoung <nhy8001@gmail.com>
2020-10-29 01:45:54 +0900

    DB에서 이미지 불러와서 shareRecyclerView로 띄우기

--- app/src/main/java/com/example/project_1/Adapter/HomeShareAdapter.java
+++ app/src/main/java/com/example/project_1/Adapter/HomeShareAdapter.java
@@ -1,6 +1,6 @@
 package com.example.project_1.Adapter;
 
-import android.media.Image;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,14 +9,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.view.menu.MenuView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.annotation.GlideModule;
+import com.example.project_1.HomeFragment;
 import com.example.project_1.HomeShareItem;
+import com.example.project_1.MainActivity;
 import com.example.project_1.R;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class HomeShareAdapter extends RecyclerView.Adapter<HomeShareAdapter.Viewhloder> {
     ArrayList<HomeShareItem> arrayList;
@@ -36,7 +41,10 @@ public class HomeShareAdapter extends RecyclerView.Adapter<HomeShareAdapter.View
 
     @Override
     public void onBindViewHolder(@NonNull HomeShareAdapter.Viewhloder holder, int position) {
-        holder.profile.setImageResource(arrayList.get(position).getProfile());
+        Glide.with(holder.itemView.getContext())
+                .load(arrayList.get(position).getProfile())
+                .into(holder.profile);
+
         holder.title.setText(arrayList.get(position).getTitle());
         holder.adress.setText(arrayList.get(position).getAdress());
 
@@ -55,13 +63,13 @@ public class HomeShareAdapter extends RecyclerView.Adapter<HomeShareAdapter.View
     }
 
     public class Viewhloder extends RecyclerView.ViewHolder {
-        protected ImageView profile;
+        protected CircleImageView profile;
         protected TextView title;
         protected TextView adress;
 
         public Viewhloder(@NonNull View itemView) {
             super(itemView);
-            this.profile = (ImageView) itemView.findViewById(R.id.profile);
+            this.profile = (CircleImageView) itemView.findViewById(R.id.profile);
             this.title = (TextView) itemView.findViewById(R.id.title);
             this.adress = (TextView) itemView.findViewById(R.id.adress);
         }
--- app/src/main/java/com/example/project_1/HomeFragment.java
+++ app/src/main/java/com/example/project_1/HomeFragment.java
@@ -1,7 +1,10 @@
 package com.example.project_1;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.service.autofill.UserData;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +27,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 import model.ShareModel;
+import model.UserModel;
 
 public class HomeFragment extends Fragment {
     //HomeBuy RecyclerView 관련 변수
@@ -41,6 +45,7 @@ public class HomeFragment extends Fragment {
 
     //Firebase
     private DatabaseReference share = FirebaseDatabase.getInstance().getReference().child("Share");
+    private DatabaseReference users = FirebaseDatabase.getInstance().getReference().child("Users");
 
 
     @Nullable
@@ -83,15 +88,29 @@ public class HomeFragment extends Fragment {
         homeShareRecyclerView.addItemDecoration(homeListDecoration);
 
             //Share DB에서 최근 5개의 글 가져와 RecyclerView로 보여주기
-        share.orderByKey().limitToLast(5).addValueEventListener(new ValueEventListener() {
+        users.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot latestShare : dataSnapshot.getChildren()){
-                    ShareModel shareModel = latestShare.getValue(ShareModel.class);
-                    HomeShareItem homeShareItem = new HomeShareItem(R.drawable.polarbear, shareModel.title, "OO동 XX아파트");
-                    shareList.add(homeShareItem);
-                }
-                homeShareAdapter.notifyDataSetChanged();
+            public void onDataChange(@NonNull DataSnapshot userDataSnapshot) {
+
+                share.orderByKey().limitToLast(5).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for(DataSnapshot latestShare : dataSnapshot.getChildren()){
+                            ShareModel shareModel = latestShare.getValue(ShareModel.class);
+
+                            UserModel userModel = userDataSnapshot.child(shareModel.host).getValue(UserModel.class);
+                            HomeShareItem homeShareItem = new HomeShareItem(userModel.imgURL, shareModel.title, "OO동 XX아파트");
+                            shareList.add(homeShareItem);
+
+                        }
+                        homeShareAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
             }
 
             @Override
@@ -99,6 +118,7 @@ public class HomeFragment extends Fragment {
 
             }
         });
+
 
 
 
