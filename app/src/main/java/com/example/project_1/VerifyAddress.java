83658b57765b9295453c3db8bf99700ecc0b1e54
Hayoung <nhy8001@gmail.com>
2020-12-03 05:30:59 +0900

    지도 중심점 현재위치로 조기화

--- app/src/main/java/com/example/project_1/VerifyAddress.java
+++ app/src/main/java/com/example/project_1/VerifyAddress.java
@@ -1,13 +1,26 @@
 package com.example.project_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.ViewGroup;
 
+import net.daum.android.map.MapViewEventListener;
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 public class VerifyAddress extends AppCompatActivity {
+
+    double currentLatitude;
+    double currentLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +28,18 @@ public class VerifyAddress extends AppCompatActivity {
         setContentView(R.layout.activity_verify__address);
 
         MapView mapView = new MapView(this);
-
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapView.setMapViewEventListener(new MapViewEventListener() {
+            @Override
+            public void onLoadMapView() {
+                mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeadingWithoutMapMoving);
+                mapView.setShowCurrentLocationMarker(true);
+                mapView.setZoomLevel(1, true);
+            }
+        });
+
         mapViewContainer.addView(mapView);
 
+        
     }
 }
